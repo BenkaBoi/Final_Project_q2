182f1c0e46fde8b7e0d19bd6d6b72a259699cc4a
Ben Kairi <benkairi7@gmail.com>
2024-03-29 13:07:39 +0000

    Implement create_table_from_csv function, after shallcheks and tests

--- q2a.sh
+++ q2a.sh
@@ -45,7 +45,10 @@ if is_mysql_installed; then
     start_mysql_service
 fi
 
+#initialize global variables 
 DB_NAME="FinalEx"
+TABLE_NAME="StudentScores"
+DATA_FILE="study_performance.csv"
 
 #create database
 create_database() {
@@ -60,4 +63,33 @@ create_database() {
     fi
 }
 
+create table from CSV headers
+create_table_from_csv() {
+    echo "Creating table ($TABLE_NAME)..."
+    local headers=$(head -1 "$DATA_FILE") #extract the headers with head -1
+    local column_definitions="id VARCHAR(255) AUTO_INCREMENT PRIMARY KEY, " #initialize column_definitions with a new column 'id' (at the original csv there is no primary key column and any identifier for each instance).
+    local column_types=("VARCHAR(255)" "VARCHAR(255)" "VARCHAR(255)" "VARCHAR(255)" "VARCHAR(255)" "INT" "INT" "INT") #defined manually the column types according to the csv column
+    local i=0 #initialize a counter for column_types index
 
+    IFS=',' read -ra ADDR <<< "$headers" #split by ',' headers' string into array - ADDR
+    for col in "${ADDR[@]}"; do #run on ADDR elements (the column names)
+        col=$(echo $col | sed -r 's/\s/_/g') #replace by using sed -r (regex) all the spaces with underscores for SQL compatibility
+        col=$(echo $col | sed -r 's/(.*)/`\1`/g') #wrap the column name with single quote for SQL compatibility
+        column_definitions+="$col ${column_types[i]}, " #concatenate all the formatted column names and their's type into column_definitions
+        ((i++))
+    done
+
+    column_definitions=${column_definitions%, } #remove the last ','
+
+    echo "CREATE TABLE $TABLE_NAME ($column_definitions);" | sudo mysql -D $DB_NAME #create the table
+
+    if [ $? -eq 0 ]; then
+        echo "Table created successfully."
+    else
+        echo "Failed to create table."
+        exit 1
+    fi
+}
+
+create_database
+create_table_from_csv
